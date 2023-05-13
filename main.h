05dae2d5e8b37a2363aaca04fd758359e68b2424
AchrafEs <achraf.es2110@gmail.com>
2023-05-13 12:59:12 +0000

    updated the header file

--- main.h
+++ main.h
@@ -11,6 +11,23 @@
 #include <fcntl.h>
 #include <errno.h>
 
-
+/* ___functions___*/
+int _putchar(char c);
+int interactive(info_t *info);
+int is_delim(char c, char *delim);
+int _isalpha(int c);
+int _atoi(char *s);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
+char *_memset(char *s, char b, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *starts_with(const char *haystack, const char *needle);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+void _puts(char *str);
 
 #endif
