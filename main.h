317e07ad71f51c3d942618f5c1cdc85324da0a8b
AchrafEs <achraf.es2110@gmail.com>
2023-05-21 12:40:27 +0000

    updated the header file

--- main.h
+++ main.h
@@ -123,4 +123,11 @@ int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
+/*_________get-line_______________*/
+ssize_t input_buf(info_t *info, char **buf, size_t *len);
+ssize_t get_input(info_t *info);
+ssize_t read_buf(info_t *info, char *buf, size_t *i);
+int _getline(info_t *info, char **ptr, size_t *length);
+void sigintHandler(__attribute__((unused))int sig_num);
+
 #endif
